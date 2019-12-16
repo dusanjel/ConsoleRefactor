578c6ba087d67fba7b934f04f3ad7fe56ed456b2
Dusan Jel <dusan_jel@hotmail.com>
2019-12-16 20:06:01 +0100

    clean TheShopCoreController

--- TheShopCore.Api/Controllers/TheShopCoreController.cs
+++ TheShopCore.Api/Controllers/TheShopCoreController.cs
@@ -11,11 +11,6 @@ namespace TheShopCore.Api.Controllers
     [Route("[controller]")]
     public class TheShopCoreController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
         private readonly ILogger<TheShopCoreController> _logger;
 
         public TheShopCoreController(ILogger<TheShopCoreController> logger)
@@ -26,14 +21,7 @@ namespace TheShopCore.Api.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return null;
         }
     }
 }

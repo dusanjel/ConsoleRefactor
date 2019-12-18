8dfaf74758f74acca5e4bc38dad09e7e0a1f3c71
Dusan Jel <dusan_jel@hotmail.com>
2019-12-18 09:38:50 +0100

    ignored file add

---
+++ TheShopCore.Utils/Log/Logger.cs
@@ -0,0 +1,69 @@
+﻿using log4net;
+using TheShopCore.Utils.Support;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheShopCore.Utils.Log
+{
+    public static class Logger
+    {
+        private static readonly ILog logger = LogManager.GetLogger(typeof(Logger));
+
+        public static void Write(Enums.LogLevel logLevel, Exception exception)
+        {
+            var logMessage = Format(exception);
+            switch (logLevel)
+            {
+                case Enums.LogLevel.Debug:
+                    logger.Debug(logMessage);
+                    break;
+                case Enums.LogLevel.Error:
+                    logger.Error(logMessage);
+                    break;
+                case Enums.LogLevel.Fatal:
+                    logger.Fatal(logMessage);
+                    break;
+                case Enums.LogLevel.Info:
+                    logger.Info(logMessage);
+                    break;
+                case Enums.LogLevel.Warn:
+                    logger.Warn(logMessage);
+                    break;
+            }
+        }
+
+        public static void Write(Enums.LogLevel logLevel, string logMessage)
+        {
+            switch (logLevel)
+            {
+                case Enums.LogLevel.Debug:
+                    logger.Debug(logMessage);
+                    break;
+                case Enums.LogLevel.Error:
+                    logger.Error(logMessage);
+                    break;
+                case Enums.LogLevel.Fatal:
+                    logger.Fatal(logMessage);
+                    break;
+                case Enums.LogLevel.Info:
+                    logger.Info(logMessage);
+                    break;
+                case Enums.LogLevel.Warn:
+                    logger.Warn(logMessage);
+                    break;
+            }
+        }
+
+        public static string Format(Exception exception)
+        {
+            return Environment.NewLine +
+               "Source: " + exception.Source + Environment.NewLine +
+               "Message: " + exception.Message + Environment.NewLine +
+               "Stack trace: " + exception.StackTrace + Environment.NewLine +
+               "Inner Exception: " + exception.InnerException;
+        }
+    }
+}
